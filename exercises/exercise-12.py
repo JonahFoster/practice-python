9312e571c71e1d463561df89973b2b71353722e4
LuckyBoop <foster.jonah@ufl.edu>
2022-12-21 22:13:19 -0500

    Create exercise-12.py

---
+++ exercises/exercise-12.py
@@ -0,0 +1,7 @@
+# Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. 
+# For practice, write this code inside a function.
+
+a = [5, 10, 15, 20, 25]
+b = []
+def listEnds():
+    return(b.append(a[0], a[len(a)-1]))
